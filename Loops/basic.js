75ac7d28248f1c8a5d411d4f24c0966378c46c55
RajPrajapati483 <rajp23509@gmail.com>
2025-07-26 12:36:14 +0530

    Loops

---
+++ Loops/basic.js
@@ -0,0 +1,14 @@
+// While Loop
+// let i = 1;
+// while(i<=10){
+//     console.log(i);
+//     i = i+1;
+// }
+// console.log("End");
+
+
+// Foor lop
+
+for(let i=1;i<=10;i++){
+    console.log(i);
+}
